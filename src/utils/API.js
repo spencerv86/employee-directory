b09e89dd716f675ecdd1a8ad6bcb3bea97920d86
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-16 21:23:25 -0500

    adjusted API call to bring 25 people

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=25&nat=gb,us";
 
 export default {
   getEmployees: function () {
-    return axios.get("https://randomuser.me/api/");
+    return axios.get(BASEURL);
   },
 };
