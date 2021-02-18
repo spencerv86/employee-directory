8e66ef1ab77e5f9d2c25ac70a59baabdf8d06c2f
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-18 00:53:49 -0500

    added buttons and ids to each of the column headers

--- src/components/Tablehead/Tablehead.jsx
+++ src/components/Tablehead/Tablehead.jsx
@@ -6,11 +6,11 @@ const Tablehead = (props) => {
       <tr>
         <th scope="col">Image</th>
         <th scope="col">
-          Name<i className="fa fa-fw fa-sort" onClick={props.handleBtnClick}></i>
+          Name<i className="fa fa-fw fa-sort" id="name" onClick={props.handleBtnClick}></i>
         </th>
-        <th scope="col">Phone</th>
-        <th scope="col">Email</th>
-        <th scope="col">DOB</th>
+        <th scope="col">Phone<i className="fa fa-fw fa-sort" id="phone" onClick={props.handleBtnClick}></i></th>
+        <th scope="col">Email<i className="fa fa-fw fa-sort" id="email" onClick={props.handleBtnClick}></i></th>
+        <th scope="col">DOB<i className="fa fa-fw fa-sort" id="DOB" onClick={props.handleBtnClick}></i></th>
       </tr>
     </thead>
   );
