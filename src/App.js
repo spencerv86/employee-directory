f2441e402f4df344f0671cf8ff069f196b282b0f
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-18 00:52:56 -0500

    adjusted the location of the background image styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header/Header";
 import Employees from "./components/Employees/Employees";
-import "./styles/App.css";
+
 
 function App() {
   return (
