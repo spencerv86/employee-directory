9bfa8cc7a40516c207f97a841106c2bc80362d72
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-18 00:53:20 -0500

    adjusted the api call so that all of the phone numbers are of the same type

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=75&nat=gb,us";
+const BASEURL = "https://randomuser.me/api/?results=75&nat=us";
 
 export default {
   getEmployees: function () {
